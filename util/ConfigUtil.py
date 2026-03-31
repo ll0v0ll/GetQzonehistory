0976929b58d3836ddcaf20a828c0ca116628883a
chaiyzh <chaiyzh@126.com>
2026-04-01 01:50:16 +0800

    fix: restore comments in ConfigUtil.py

--- util/ConfigUtil.py
+++ util/ConfigUtil.py
@@ -11,27 +11,34 @@ def save_user(cookies):
 
 
 def init_flooder():
+    # 初始化temp文件夹
     if not os.path.exists(temp_path):
         os.makedirs(temp_path)
         print(f"Created directory: {temp_path}")
 
+    # 初始化user文件夹
     if not os.path.exists(user_path):
         os.makedirs(user_path)
         print(f"Created directory: {user_path}")
 
+    # 初始化result文件夹
     if not os.path.exists(result_path):
         os.makedirs(result_path)
         print(f"Created directory: {result_path}")
 
 
 def read_files_in_folder():
+    # 获取文件夹下的所有文件
     files = os.listdir(user_path)
+    # 如果文件夹为空
     if not files:
         return None
+    # 输出文件列表
     print("已登录用户列表:")
     for i, file in enumerate(files):
         print(f"{i + 1}. {file}")
 
+    # 选择文件
     while True:
         try:
             choice = int(input("请选择要登录的用户序号，重新登录输入0: "))
@@ -44,6 +51,7 @@ def read_files_in_folder():
         except ValueError:
             print("无效的选择，请重新输入。")
 
+    # 读取选择的文件
     selected_file = files[choice - 1]
     file_path = os.path.join(user_path, selected_file)
     with open(file_path, 'r') as file:
